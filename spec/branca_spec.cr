a339051f752a0235e6f8412dfcf90d74f7f131c6
Johannes Rabausch <mail@jrabausch.de>
2023-05-30 16:22:32 +0000

    Added all required test vectors

--- spec/branca_spec.cr
+++ spec/branca_spec.cr
@@ -1,101 +1,156 @@
 require "./spec_helper"
 
+# Branca test vectors
+#
+# https://github.com/tuupola/branca-spec/blob/master/test_vectors.json
 describe Branca do
-  # https://github.com/tuupola/branca-spec/blob/master/test_vectors.json
-  # test vectors taken from
-  # https://github.com/tuupola/branca-php/blob/2.x/tests/BrancaTest.php
-  it "should throw on invalid key" do
-    expect_raises(Exception, "secret key has to be 32 bytes strong") do
-      Branca.new "notsosecretkey".to_slice
+  branca = Branca.new "73757065727365637265746b6579796f7573686f756c646e6f74636f6d6d6974".hexbytes
+
+  it "throws with invalid key" do
+    key = "746f6f73686f72746b6579".hexbytes
+    expect_raises(Exception) do
+      Branca.new key
     end
   end
 
-  branca = Branca.new "supersecretkeyyoushouldnotcommit".to_slice
+  describe "#encode" do
+    it "encodes hello world with zero timestamp" do
+      payload = "48656c6c6f20776f726c6421".hexbytes
+      token = branca.encode(payload, 0)
+      token.should eq "870S4BYxgHw0KnP3W9fgVUHEhT5g86vJ17etaC5Kh5uIraWHCI1psNQGv298ZmjPwoYbjDQ9chy2z"
+    end
+    it "encodes hello world with max timestamp" do
+      payload = "48656c6c6f20776f726c6421".hexbytes
+      token = branca.encode(payload, 4294967295)
+      token.should eq "89i7YCwu5tWAJNHUDdmIqhzOi5hVHOd4afjZcGMcVmM4enl4yeLiDyYv41eMkNmTX6IwYEFErCSqr"
+    end
+    it "encodes hello world with November 27 timestamp" do
+      payload = "48656c6c6f20776f726c6421".hexbytes
+      token = branca.encode(payload, 123206400)
+      token.should eq "875GH23U0Dr6nHFA63DhOyd9LkYudBkX8RsCTOMz5xoYAMw9sMd5QwcEqLDRnTDHPenOX7nP2trlT"
+    end
+    it "encodes eight null bytes with zero timestamp" do
+      payload = "0000000000000000".hexbytes
+      token = branca.encode(payload, 0)
+      token.should eq "1jIBheHbDdkCDFQmtgw4RUZeQoOJgGwTFJSpwOAk3XYpJJr52DEpILLmmwYl4tjdSbbNqcF1"
+    end
+    it "encodes eight null bytes with max timestamp" do
+      payload = "0000000000000000".hexbytes
+      token = branca.encode(payload, 4294967295)
+      token.should eq "1jrx6DUu5q06oxykef2e2ZMyTcDRTQot9ZnwgifUtzAphGtjsxfbxXNhQyBEOGtpbkBgvIQx"
+    end
+    it "encodes eight null bytes with November 27th timestamp" do
+      payload = "0000000000000000".hexbytes
+      token = branca.encode(payload, 123206400)
+      token.should eq "1jJDJOEjuwVb9Csz1Ypw1KBWSkr0YDpeBeJN6NzJWx1VgPLmcBhu2SbkpQ9JjZ3nfUf7Aytp"
+    end
+    it "encodes empty payload" do
+      payload = "".hexbytes
+      token = branca.encode(payload, 0)
+      token.should eq "4sfD0vPFhIif8cy4nB3BQkHeJqkOkDvinI4zIhMjYX4YXZU5WIq9ycCVjGzB5"
+    end
+    it "encodes non-UTF8 payload" do
+      payload = "80".hexbytes
+      token = branca.encode(payload, 123206400)
+      token.should eq "K9u6d0zjXp8RXNUGDyXAsB9AtPo60CD3xxQ2ulL8aQoTzXbvockRff0y1eXoHm"
+    end
+  end
 
   describe "#decode" do
-    it "should decode Hello world! with zero timestamp" do
+    it "decodes hello world with zero timestamp" do
       token = "870S4BYxgHw0KnP3W9fgVUHEhT5g86vJ17etaC5Kh5uIraWHCI1psNQGv298ZmjPwoYbjDQ9chy2z"
       payload, timestamp = branca.decode(token)
-      str = String.new(payload)
-      str.should eq "Hello world!"
+      payload.hexstring.should eq "48656c6c6f20776f726c6421"
       timestamp.should eq 0
     end
-    it "should decode Hello world! with max timestamp" do
+    it "decodes hello world with max timestamp" do
       token = "89i7YCwu5tWAJNHUDdmIqhzOi5hVHOd4afjZcGMcVmM4enl4yeLiDyYv41eMkNmTX6IwYEFErCSqr"
       payload, timestamp = branca.decode(token)
-      str = String.new(payload)
-      str.should eq "Hello world!"
+      payload.hexstring.should eq "48656c6c6f20776f726c6421"
       timestamp.should eq 4294967295
     end
-    it "should decode Hello world! with nov 27 timestamp" do
+    it "decodes hello world with November 27 timestamp" do
       token = "875GH23U0Dr6nHFA63DhOyd9LkYudBkX8RsCTOMz5xoYAMw9sMd5QwcEqLDRnTDHPenOX7nP2trlT"
       payload, timestamp = branca.decode(token)
-      str = String.new(payload)
-      str.should eq "Hello world!"
+      payload.hexstring.should eq "48656c6c6f20776f726c6421"
       timestamp.should eq 123206400
     end
-    it "should decode eight null bytes with zero timestamp" do
+    it "decodes eight null bytes with zero timestamp" do
       token = "1jIBheHbDdkCDFQmtgw4RUZeQoOJgGwTFJSpwOAk3XYpJJr52DEpILLmmwYl4tjdSbbNqcF1"
       payload, timestamp = branca.decode(token)
-      hex = payload.hexstring
-      hex.should eq "0000000000000000"
+      payload.hexstring.should eq "0000000000000000"
       timestamp.should eq 0
     end
-    it "should decode eight null bytes with max timestamp" do
+    it "decodes eight null bytes with max timestamp" do
       token = "1jrx6DUu5q06oxykef2e2ZMyTcDRTQot9ZnwgifUtzAphGtjsxfbxXNhQyBEOGtpbkBgvIQx"
       payload, timestamp = branca.decode(token)
-      hex = payload.hexstring
-      hex.should eq "0000000000000000"
+      payload.hexstring.should eq "0000000000000000"
       timestamp.should eq 4294967295
     end
-    it "should decode eight null bytes with nov 27 timestamp" do
+    it "decodes eight null bytes with November 27th timestamp" do
       token = "1jJDJOEjuwVb9Csz1Ypw1KBWSkr0YDpeBeJN6NzJWx1VgPLmcBhu2SbkpQ9JjZ3nfUf7Aytp"
       payload, timestamp = branca.decode(token)
-      hex = payload.hexstring
-      hex.should eq "0000000000000000"
+      payload.hexstring.should eq "0000000000000000"
       timestamp.should eq 123206400
     end
-    it "should decode empty payload with zero timestamp" do
+    it "decodes empty payload" do
       token = "4sfD0vPFhIif8cy4nB3BQkHeJqkOkDvinI4zIhMjYX4YXZU5WIq9ycCVjGzB5"
       payload, timestamp = branca.decode(token)
-      str = String.new(payload)
-      str.should eq ""
+      payload.hexstring.should eq ""
       timestamp.should eq 0
     end
-    it "should decode non utf-8 chars with nov 27 timestamp" do
+    it "decodes non-UTF8 payload" do
       token = "K9u6d0zjXp8RXNUGDyXAsB9AtPo60CD3xxQ2ulL8aQoTzXbvockRff0y1eXoHm"
       payload, timestamp = branca.decode(token)
-      hex = payload.hexstring
-      hex.should eq "80"
+      payload.hexstring.should eq "80"
       timestamp.should eq 123206400
     end
-    it "should throw with wrong token version" do
+    it "throws with wrong version 0xBB" do
       token = "89mvl3RkwXjpEj5WMxK7GUDEHEeeeZtwjMIOogTthvr44qBfYtQSIZH5MHOTC0GzoutDIeoPVZk3w"
       expect_raises(Exception) do
         branca.decode(token)
       end
     end
-    it "should throw with modified token nonce" do
+    it "throws with invalid base62 characters" do
+      token = "875GH23U0Dr6nHFA63DhOyd9LkYudBkX8RsCTOMz5xoYAMw9sMd5QwcEqLDRnTDHPenOX7nP2trlT_"
+      expect_raises(Exception) do
+        branca.decode(token)
+      end
+    end
+    it "throws with modified version" do
+      token = "89mvl3S0BE0UCMIY94xxIux4eg1w5oXrhvCEXrDAjusSbO0Yk7AU6FjjTnbTWTqogLfNPJLzecHVb"
+      expect_raises(Exception) do
+        branca.decode(token)
+      end
+    end
+    it "throws with modified first byte of the nonce" do
       token = "875GH233SUysT7fQ711EWd9BXpwOjB72ng3ZLnjWFrmOqVy49Bv93b78JU5331LbcY0EEzhLfpmSx"
       expect_raises(Exception) do
         branca.decode(token)
       end
     end
-    it "should throw with modified chiphertext" do
+    it "throws with modified timestamp" do
+      token = "870g1RCk4lW1YInhaU3TP8u2hGtfol16ettLcTOSoA0JIpjCaQRW7tQeP6dQmTvFIB2s6wL5deMXr"
+      expect_raises(Exception) do
+        branca.decode(token)
+      end
+    end
+    it "throws with modified last byte of the ciphertext" do
       token = "875GH23U0Dr6nHFA63DhOyd9LkYudBkX8RsCTOMz5xoYAMw9sMd5Qw6Jpo96myliI3hHD7VbKZBYh"
       expect_raises(Exception) do
         branca.decode(token)
       end
     end
-    it "should throw with modified poly1305 tag" do
+    it "throws with modified last byte of the Poly1305 tag" do
       token = "875GH23U0Dr6nHFA63DhOyd9LkYudBkX8RsCTOMz5xoYAMw9sMd5QwcEqLDRnTDHPenOX7nP2trk0"
       expect_raises(Exception) do
         branca.decode(token)
       end
     end
-    it "should throw with wrong secret key" do
+    it "throws with wrong key" do
       token = "870S4BYxgHw0KnP3W9fgVUHEhT5g86vJ17etaC5Kh5uIraWHCI1psNQGv298ZmjPwoYbjDQ9chy2z"
-      br = Branca.new "wrongsecretkeyyoushouldnotcommit".to_slice
+      key = "77726f6e677365637265746b6579796f7573686f756c646e6f74636f6d6d6974".hexbytes
+      br = Branca.new key
       expect_raises(Exception) do
         br.decode(token)
       end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,10 @@
 require "spec"
 require "../src/branca"
+
+# Branca Test Object
+#
+# This sets a user defined nonce for testing.
+# Do not do this in production.
+struct Branca
+  @nonce = "beefbeefbeefbeefbeefbeefbeefbeefbeefbeefbeefbeef".hexbytes
+end
